1119c467a2b84a6bc68081c6ecb22ada1e286690
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-10-01 05:38:19 +0530

    Set to store unique categories, filteritems to filter menus based on category

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-import React, { useState } from 'react';
-import Menu from './Menu';
-import Categories from './Categories';
-import items from './data';
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
+
+let uniqueCategories = new Set();
+for (const iterator of items) {
+  uniqueCategories.add(iterator.category);
+}
 
 function App() {
-  return <h2>menu project setup</h2>;
+
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories] = useState(['all',...uniqueCategories]);
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(items);
+    }
+    else {
+      const newItems = items.filter((item) => item.category === category);
+      setMenuItems(newItems);
+    }
+  };
+
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+          <Categories filterItems={filterItems} categories={categories} />
+          <Menu items={menuItems} />
+        </div>
+      </section>
+    </main>
+  );
 }
 
 export default App;
